43d0d73bc856a00fb7197794124a857d4c9b8f05
Slindile <slnkabijhb024@student.wethinkcode.co.za>
2025-04-23 19:47:33 +0200

    encapsulation student class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Slindile");
+        student.setAge(21);
+        student.setScore(90.5);
+
+        student.displayinfo();
+
+    }
+}

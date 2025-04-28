a56d242a9c9d4aae5730a7cfd9b6afedf5f57e5c
Slindile <slindilenkabs23@gmail.com>
2025-04-29 00:09:22 +0200

    Solved question1 of abstraction

--- src/main/java/abstraction/question1/Main.java
+++ src/main/java/abstraction/question1/Main.java
@@ -1,4 +1,8 @@
 package abstraction.question1;
 
 public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2);
+        System.out.println(circle.getArea());
+    }
 }

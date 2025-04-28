37a88473f2e0d20390b4700bb4d12e880bbe3856
Slindile <slindilenkabs23@gmail.com>
2025-04-29 00:10:15 +0200

    Solved question1 of abstraction

--- src/main/java/abstraction/question1/Shape.java
+++ src/main/java/abstraction/question1/Shape.java
@@ -1,4 +1,12 @@
 package abstraction.question1;
 
-public class Shape {
+public abstract class Shape {
+
+    public abstract double getArea();
+
+    public void display(){
+        System.out.println("Calculating area ...");
+
+
+    }
 }

ff8886a44bde9f266d7d6539a54161fd1f3c99de
Slindile <slindilenkabs23@gmail.com>
2025-05-01 01:49:55 +0200

    Fixed main

--- src/main/java/composition_interface/question1/Main.java
+++ src/main/java/composition_interface/question1/Main.java
@@ -1,4 +1,8 @@
 package composition_interface.question1;
 
 public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("The Obstacle is the way");
+        book.print();
+    }
 }

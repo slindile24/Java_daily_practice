7322684f7e13f6d8d459e891ad4fe819b6abb0d3
Slindile <slindilenkabs23@gmail.com>
2025-05-01 01:49:06 +0200

    Fixed Book.java

--- src/main/java/composition_interface/question1/Book.java
+++ src/main/java/composition_interface/question1/Book.java
@@ -1,4 +1,15 @@
 package composition_interface.question1;
+import java.util.Scanner;
 
-public class Book {
+public class Book implements Printable{
+    private String title;
+
+    public Book(String title){
+        this.title = title;
+
+    }
+    @Override
+    public void print() {
+        System.out.println("Printing book: " + title);
+    }
 }

33c21e8c7684b1a16d4da907cf2edb9026541168
Slindile <slindilenkabs23@gmail.com>
2025-05-07 18:58:19 +0200

    Basic Scanner check even or odd number

---
+++ src/main/java/Scanner_Practice/question1/Even_Or_Odd.java
@@ -0,0 +1,17 @@
+package Scanner_Practice.question1;
+import java.util.Scanner;
+
+public class Even_Or_Odd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0){
+            System.out.println("This is an even number.");
+        }else{
+            System.out.println("This an odd number ");
+        }
+
+
+    }
+}

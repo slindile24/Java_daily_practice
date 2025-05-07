7ef4fecc6daa34101f2564d49b76da16bb5de96d
Slindile <slindilenkabs23@gmail.com>
2025-05-07 18:37:25 +0200

    Scanner basic completed

--- src/main/java/OOP/question3/Main.java
+++ src/main/java/OOP/question3/Main.java
@@ -2,10 +2,11 @@ package OOP.question3;
 
 public class Main {
     public static void main(String[] args) {
-        SavingsAccount savingsAccount = new SavingsAccount("12345",1000);
-        CheckingAccount checkingAccount = new CheckingAccount("12345",1000);
+        SavingsAccount savingsAccount = new SavingsAccount("12345",2000);
+        CheckingAccount checkingAccount = new CheckingAccount("12345",2000);
         savingsAccount.withdraw(120);
         savingsAccount.deductTax();
+        checkingAccount.withdraw(10000);
 
     }
 }
--- src/main/java/OOP/question3/Taxable.java
+++ src/main/java/OOP/question3/Taxable.java
@@ -3,5 +3,6 @@ package OOP.question3;
 public interface Taxable {
     default void deductTax(){
 
+
     }
 }

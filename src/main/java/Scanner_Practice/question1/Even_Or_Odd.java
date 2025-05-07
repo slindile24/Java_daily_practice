d9b0a3c630c33c642a23746fde54c0788cb73e3e
Slindile <slindilenkabs23@gmail.com>
2025-05-08 00:08:42 +0200

    added try-catch for error handling

--- src/main/java/Scanner_Practice/question1/Even_Or_Odd.java
+++ src/main/java/Scanner_Practice/question1/Even_Or_Odd.java
@@ -1,15 +1,22 @@
 package Scanner_Practice.question1;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Even_Or_Odd {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = scanner.nextInt();
-        if (number % 2 == 0){
-            System.out.println("This is an even number.");
-        }else{
-            System.out.println("This an odd number ");
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            if (number % 2 == 0){
+                System.out.println("This is an even number.");
+            }else{
+                System.out.println("This an odd number ");
+            }
+            scanner.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Enter a valid integer!");;
         }
 
 

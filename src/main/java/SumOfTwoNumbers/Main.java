ce87c466b13e4eb7587a2eb0762053a0b7cd3863
Slindile <slindilenkabs23@gmail.com>
2025-05-10 23:39:01 +0200

    Solutions of sumOftwoNumbers completed

---
+++ src/main/java/SumOfTwoNumbers/Main.java
@@ -0,0 +1,21 @@
+package SumOfTwoNumbers;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter first number: ");
+            int firstNumber = scanner.nextInt();
+            System.out.println("Enter second number: ");
+            int secondNumber = scanner.nextInt();
+            int result = firstNumber + secondNumber;
+            System.out.println("Result is: " + result);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Enter a valid integer!");
+        }
+    }
+}

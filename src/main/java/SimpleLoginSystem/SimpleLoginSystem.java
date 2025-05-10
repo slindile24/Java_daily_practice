74b4498314f1683e2bb157063f80db2608938378
Slindile <slindilenkabs23@gmail.com>
2025-05-11 00:13:23 +0200

    SimpleLoginSystem solutions completed

--- src/main/java/SimpleLoginSystem/SimpleLoginSystem.java
+++ src/main/java/SimpleLoginSystem/SimpleLoginSystem.java
@@ -1,4 +1,23 @@
 package SimpleLoginSystem;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class SimpleLoginSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username: ");
+        String EnteredUsername = scanner.nextLine();
+        System.out.println("Enter your password: ");
+        String EnteredPassword = scanner.nextLine();
+        String correctUsername = "Slindile";
+        String correctPassword = "Slishh";
+
+        if(Objects.equals(EnteredPassword, correctPassword) && Objects.equals(EnteredUsername, correctUsername)){
+            System.out.println("Access granted.");
+        }else{
+            System.out.println("Access denied.");
+        }
+
+    }
 }
